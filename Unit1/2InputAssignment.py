f2cb31216733c822f445ccc811b7b1bb26eca730
Adrian Sanders <thenestoutside@outlook.com>
2024-03-28 13:00:10 -0500

    assignment 2

--- Unit1/2InputAssignment.py
+++ Unit1/2InputAssignment.py
@@ -1,15 +1,30 @@
 print("########## 1.2.1 ##########")
+name1 = input("What is your name?" + " ")
+print(name1 + "\n" + name1)
 
 print("########## 1.2.2 ##########")
-
+name2 = input("What is your name?" + " ")
+print("!" + name1 + "!" + name1 + "!")
 print("########## 1.2.3 ##########")
+
+firstname = input("Enter your First name: ")
+lastname = input("Enter your Last name: ")
+street = input("Enter your street address: ")
+addcity = input("Enter your address city: ")
+addstate = input("Enter your address state: ")
+addzip = input("Add your zip code: ")
+print(firstname + " " + lastname + "\n" + street + "\n" + addcity + ", " + addstate + "\n" + addzip)
 
 print("########## 1.2.4 ##########")
 
 # Fix the code
 part = input("The 1st part: ")
-part = input("The 1st part: ")
-part = input("The 1st part: ")
-print(part + part + part)
+part2 = input("The 2nd part: ")
+part3 = input("The 3rd part: ")
+print(part + "-" + part2 + "-" + part3 + "!")
 
 print("########## 1.2.5 ##########")
+
+storyname = input("Please type in a name: ")
+storyear = input("Please enter a year: ")
+print(storyname + " is a valiant knight...born in the year " + storyear + ". One morning " + storyname + " woke up to an awful racket: a dragon was approaching the village. Only " + storyname + " could save the village's residents.")
