eccb636bd26bee1acd70b75e276ab820f9ae8fd7
Adrian Sanders <thenestoutside@outlook.com>
2024-04-02 12:05:02 -0500

    assignment

--- Unit1/4ArithmeticAssignment.py
+++ Unit1/4ArithmeticAssignment.py
@@ -1,23 +1,68 @@
-print("########## 1.4.1 ##########")
+# print("########## 1.4.1 ##########")
+# num1 = int(input("please enter a number: "))
+# num2 = (num1) * 5
+# print(num2)
 
-print("########## 1.4.2 ##########")
+# print("########## 1.4.2 ##########")
+# name = input("Enter your name: ")
+# age = int(input("Enter your birth year: "))
+# age2 = 2024-age
+# print("Hello, " + name + ", you will be", age2, "at the end of 2024.")
 
-print("########## 1.4.3 ##########")
+# print("########## 1.4.3 ##########")
+# days = int(input("How many days?"))
+# sec = int(days * 24 * 60 * 60)
+# print("Seconds in that many days:", sec)
 
-print("########## 1.4.4 ##########")
-# Fix the code
-number = int(input("Please type in the first number: "))
-number = int(input("Please type in the second number: "))
-number = int(input("Please type in the third number: "))
 
-product = number * number * number
+# print("########## 1.4.4 ##########")
+# # Fix the code
+# number1 = int(input("Please type in the first number: "))
+# number2 = int(input("Please type in the second number: "))
+# number3 = int(input("Please type in the third number: "))
 
-print("The product is", product)
+# product = number1 * number2 * number3
 
-print("########## 1.4.5 ##########")
+# print("The product is", product)
 
-print("########## 1.4.6 ##########")
+# print("########## 1.4.5 ##########")
+# num2 = int(input("Please type in the first number: "))
+# num3 = int(input("Please type in the second number: "))
+# sum = num2 + num3
+# prod1 = num2 * num3
+# print ("Number 1:", num2)
+# print ("Number 2:", num3)
+# print ("Sum:", sum)
+# print ("Product:", prod1)
 
-print("########## 1.4.7 ##########")
+# print("########## 1.4.6 ##########")
+# num1 = int(input("Please type in the first number: "))
+# num2 = int(input("Please type in the second number: "))
+# num3 = int(input("Please type in the third number: "))
+# num4 = int(input("Please type in the fourth number: "))
+
+# sum = num1 + num2 + num3 + num4
+# mean = num1 + num2 + num3 + num4
+# mean = mean/4
+# print ("Number 1:",  num1)
+# print ("Number 2:",  num2)
+# print ("Number 3:",  num3)
+# print ("Number 4:",  num4)
+# print ("The sum is", sum, "and the mean is", mean)
+
+# print("########## 1.4.7 ##########")
+# cafe = int(input("How many times a week do you eat at the student cafeteria? "))
+# lunch = float(input("The price of a typical student lunch? "))
+# grocery = float(input("How much money do you spend on groceries in a week? "))
+# totcafe = cafe + lunch
+# daily = lunch + (grocery / 7)
+# weekly = totcafe + grocery
+# print ("Average food expenditure:")
+# print ("Daily: $" + str(daily))
+# print ("Weekly: $" + str(weekly))
 
 print("########## 1.4.8 ##########")
+numb = int(input("How many students are there? "))
+sizewant = int(input("Desired group size? "))
+size = numb / sizewant
+print ("Number of groups formed:", size)
